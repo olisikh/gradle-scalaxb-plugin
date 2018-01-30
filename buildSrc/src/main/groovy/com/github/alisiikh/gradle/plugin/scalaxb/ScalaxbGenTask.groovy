0845f7a4bc1476e4c9bb14ae4b39aec5efd743a9
Oleksii Lisikh <lizard5472@gmail.com>
2018-01-31 00:15:27 +0100

    Isolate scalaxb classes generation from gradle plugin by creating additional runtime classpath

---
+++ buildSrc/src/main/groovy/com/github/alisiikh/gradle/plugin/scalaxb/ScalaxbGenTask.groovy
@@ -0,0 +1,37 @@
+package com.github.alisiikh.gradle.plugin.scalaxb
+
+import org.gradle.api.tasks.JavaExec
+
+class ScalaxbGenTask extends JavaExec {
+
+    ScalaxbGenTask() {
+    }
+
+    void exec() {
+        def ext = project.extensions.getByType(ScalaxbExtension)
+
+        project.configurations.scalaxbRuntime.each { println it }
+
+        def xsdFiles = ext.xsdDir.listFiles(new FileFilter() {
+            @Override
+            boolean accept(File file) {
+                return file.name.endsWith(".xsd")
+            }
+        })
+
+        xsdFiles.each { xsdFile ->
+            project.javaexec {
+                classpath = project.configurations.scalaxbRuntime
+                main = 'scalaxb.compiler.Main'
+
+                systemProperties = System.properties as Map
+                standardInput = System.in
+                standardOutput = System.out
+
+                args xsdFile.absolutePath
+                args "-p", ext.packageName
+                args "-d", ext.destDir
+            }
+        }
+    }
+}
--- buildSrc/src/main/groovy/com/github/alisiikh/gradle/plugin/scalaxb/ScalaxbPlugin.groovy
+++ buildSrc/src/main/groovy/com/github/alisiikh/gradle/plugin/scalaxb/ScalaxbPlugin.groovy
@@ -23,23 +23,11 @@ package com.github.alisiikh.gradle.plugin.scalaxb
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.scala.ScalaPlugin
-import scalaxb.compiler.Config
-import scalaxb.compiler.Config$
-import scalaxb.compiler.ConfigEntry
-import scalaxb.compiler.Module
-
-import static scala.collection.JavaConverters.asScalaBuffer
-import static scala.collection.JavaConverters.mapAsScalaMap
-import static scalaxb.compiler.Module$.MODULE$ as Module
-import static scala.collection.Map$.MODULE$ as ScalaMap
-import static scala.Option$.MODULE$ as Option
-import static scala.Some$.MODULE$ as Some
+import org.gradle.api.internal.ConventionTask
 
 class ScalaxbPlugin implements Plugin<Project> {
-
-    private def SCALAXB_EXT_NAME = 'scalaxb'
-    private def SCALAXB_TASK_NAME = 'generateScalaxb'
+    private final def SCALAXB_EXT_NAME = 'scalaxb'
+    private final def SCALAXB_TASK_NAME = 'generateScalaxb'
 
     private Project project
 
@@ -47,47 +35,36 @@ class ScalaxbPlugin implements Plugin<Project> {
     void apply(Project project) {
         this.project = project
 
-        project.pluginManager.apply ScalaPlugin
-
         def scalaxbExt = project.extensions.create(SCALAXB_EXT_NAME, ScalaxbExtension, project) as ScalaxbExtension
 
-        project.task(SCALAXB_TASK_NAME) {
-            group = 'scalaxb'
-
-            doLast {
-                scalaxbExt.destDir?.mkdirs()
-
-                Module module = Module.moduleByFileName(scalaxbExt.xsdDir)
-
-                def xsdFiles = collectXsdFiles(scalaxbExt)
-
-                Config config = buildConfig(scalaxbExt)
-                module.processFiles(asScalaBuffer(Arrays.asList(xsdFiles)), config)
-            }
-        }
-        project.tasks.findByName('compileScala').dependsOn(SCALAXB_TASK_NAME)
+        createConfiguration()
+        createTasks(scalaxbExt)
     }
 
-    private Config buildConfig(ScalaxbExtension scalaxbExt) {
-        def packages = ScalaMap.apply(mapAsScalaMap([
-                (Option.empty()): Some.apply(scalaxbExt.packageName)
-        ]).toSeq())
+    def createConfiguration() {
+        project.configurations {
+            create('scalaxbRuntime') {
+                visible = false
+            }
+        }
 
-        [
-                ConfigEntry.GeneratePackageDir$.MODULE$,
-                new ConfigEntry.Outdir(scalaxbExt.destDir),
-                new ConfigEntry.PackageNames(packages)
-        ].inject(Config$.MODULE$['default'] as Config) { Config cfg, ConfigEntry cfgItem ->
-            cfg.update(cfgItem)
+        project.afterEvaluate { p ->
+            def scalaxbDependency = p.configurations.scalaxbRuntime.find { it.name.matches("scalaxb_.*-.*\\.jar") }
+            if (!scalaxbDependency) {
+                scalaxbRuntime "org.scalaxb:scalaxb_2.12:2.12.3"
+            }
         }
     }
 
-    private File[] collectXsdFiles(ScalaxbExtension scalaxbExt) {
-        scalaxbExt.xsdDir.listFiles(new FilenameFilter() {
-            @Override
-            boolean accept(File dir, String name) {
-                name.endsWith('.xsd')
-            }
-        })
+    def createTasks(ScalaxbExtension scalaxbExt) {
+        def scalaxbGenTask = project.tasks.create(
+                name: SCALAXB_TASK_NAME,
+                type: ScalaxbGenTask,
+                description: "Generate scalaxb sources", group: "scalaxb"
+        ) as ConventionTask
+
+        scalaxbGenTask.convention.plugins["scalaxb"] = scalaxbExt
+
+        project.tasks.findByName('compileScala').dependsOn(scalaxbGenTask)
     }
 }
